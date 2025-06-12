a58a1096a244bd1ec92b297922e6f38fe2f9935e
Wojciech <wkieliszek@student.agh.edu.pl>
2025-06-12 11:07:52 +0200

    CICD test28

--- project1-react/src/features/todo/TodoForm.tsx
+++ project1-react/src/features/todo/TodoForm.tsx
@@ -94,8 +94,10 @@ export const TodoForm = () => {
                             dateFormat="YYYY-MM-DD"
                             timeFormat="HH:mm"
                             onChange={(value) => {
-                                form.setFieldValue("deadline", value.toString());
-                            }}
+                                if (value instanceof Date && !isNaN(value.getTime())) {
+                                    form.setFieldValue("deadline", value);
+    }
+}}
                         />
                     </div>
 
